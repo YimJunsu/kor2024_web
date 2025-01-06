40c3ea49d53a2300b69cac39b920a5b639458c89
YimJunsu <yimjunsu@gmail.com>
2025-01-06 21:23:06 +0900

    day56&어노테이션

---
+++ src/main/java/day56/RestController1.java
@@ -0,0 +1,32 @@
+package day56;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller // 해당 클래스에 어노테이션[미리 코드가 담긴 스티커] 주입
+public class RestController1 {
+
+    @GetMapping("/day56/rest1") // [GET] http://localhost:8080/day56/rest1
+    @ResponseBody
+    public String getRest1(){
+        return "HI 클라이언트, 나는 GET Rest1 이야.";
+    }
+
+    @PostMapping("/day56/rest1") // [POST] http://localhost:8080/day56/rest1
+    @ResponseBody
+    public String postRest1(){
+        return "안녕 클라이먼트, 나는 POST Rest1 이야.";
+    }
+
+    @PutMapping("/day56/rest1") // [PUT] http://localhost:8080/day56/rest1
+    @ResponseBody
+    public String putRest1(){
+        return "안녕 클라이언트, 나는 PUT Rest1 이야.";
+    }
+
+    @DeleteMapping("/day56/rest1") // [DELETE] http://localhost:8080/day56/rest1
+    @ResponseBody
+    public String deleteRest1(){
+        return "안녕 클라이언트, 나는 DELETE Rest1 이야.";
+    }
+}
---
+++ src/main/java/day56/RestController3.java
@@ -0,0 +1,20 @@
+package day56;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/day56/rest3")
+public class RestController3 {
+
+    @GetMapping("")
+    public boolean getRest3(){return true;}
+
+    @PostMapping("")
+    public boolean postRest3(){return false;}
+
+    @PutMapping("")
+    public boolean putRest3(){return true;}
+
+    @DeleteMapping("")
+    public boolean deleteRest3(){return false;}
+}
