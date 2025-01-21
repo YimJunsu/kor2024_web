8bf1b49ed808de9935616f22c8629f8a4c2a463a
Yim_JunSu <yimjunsu@gmail.com>
2025-01-21 20:20:01 +0900

    .

---
+++ src/main/resources/data.sql
@@ -0,0 +1,22 @@
+
+insert into member ( mid , mpwd , mname , mphno , maddress ) values ( "aa01" , "1234" , "김김김" , "010-1111-1111" , "서울" );
+insert into member ( mid , mpwd , mname , mphno , maddress ) values ( "aa02" , "4567" , "님님님" , "010-2222-2222" , "경기" );
+insert into member ( mid , mpwd , mname , mphno , maddress ) values ( "aa03" , "7890" , "딤딤딤" , "010-3333-3333" , "인천" );
+insert into member ( mid , mpwd , mname , mphno , maddress ) values ( "aa04" , "0123" , "림림림" , "010-4444-4444" , "제주" );
+insert into member ( mid , mpwd , mname , mphno , maddress ) values ( "aa05" , "3456" , "밈밈밈" , "010-5555-5555" , "부산" );
+
+insert into product ( pname , price , stock ) values( "소고기" , "10000" , 1);
+insert into product ( pname , price , stock ) values( "당근" , "11000" , 2);
+insert into product ( pname , price , stock ) values( "참이슬" , "12000" , 3);
+insert into product ( pname , price , stock ) values( "파워에이드" , "13000" , 4);
+insert into product ( pname , price , stock ) values( "빵" , "14000" , 5);
+
+insert into category ( cname ) values( "육류" );
+insert into category ( cname ) values( "채소" );
+insert into category ( cname ) values( "주류" );
+insert into category ( cname ) values( "음료" );
+insert into category ( cname ) values( "베이커리" );
+
+insert into orders ( ocount ) values( 2 );
+
+
